bc2be95cbc1ca51361d7862c87b8a7c6ef6599b6
Prashant Ingale <ingaleprashant8@gmail.com>
2018-08-20 13:11:40 +0530

    Test Commit

--- prjWebPOS/src/com/sanguine/webpos/controller/clsPOSAreaMasterController.java
+++ prjWebPOS/src/com/sanguine/webpos/controller/clsPOSAreaMasterController.java
@@ -1,6 +1,5 @@
 package com.sanguine.webpos.controller;
 
-import java.lang.reflect.Array;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,10 +27,13 @@ import com.sanguine.webpos.util.clsPOSUtilityController;
 
 @Controller
 public class clsPOSAreaMasterController {
+	
 	@Autowired
 	private clsGlobalFunctions objGlobal;
+	
 	@Autowired
 	private clsPOSGlobalFunctionsController objPOSGlobal;
+	
 	@Autowired 
 	private clsBaseServiceImpl objBaseServiceImpl;
 	
@@ -41,10 +43,10 @@ public class clsPOSAreaMasterController {
 	@Autowired
 	clsPOSMasterService objMasterService;
 	
-	Map map=new HashMap();
 	@RequestMapping(value = "/frmPOSAreaMaster", method = RequestMethod.GET)
 	public ModelAndView funOpenForm(@ModelAttribute("command") @Valid clsPOSAreaMasterBean objBean,BindingResult result,Map<String,Object> model, HttpServletRequest request)throws Exception
 	{
+		Map map=new HashMap();
 		String urlHits = "1";
 		try {
 			urlHits = request.getParameter("saddr").toString();
@@ -54,18 +56,15 @@ public class clsPOSAreaMasterController {
 		model.put("urlHits", urlHits);
 		
 		String clientCode=request.getSession().getAttribute("gClientCode").toString();
-		
 		List list=objMasterService.funFillPOSCombo(clientCode);
-		
 		map.put("All", "All");
+		
 		for(int cnt=0;cnt<list.size();cnt++)
 		{
 			Object[] obj=(Object[]) list.get(cnt);
-			
 			map.put(obj[0].toString(), obj[1].toString());
 		}
 		model.put("posList", map);
-		
 		
 		if("2".equalsIgnoreCase(urlHits)){
 			return new ModelAndView("frmPOSAreaMaster_1");
@@ -74,8 +73,8 @@ public class clsPOSAreaMasterController {
 		}else {
 			return null;
 		}
-		 
 	}
+	
 	
 	@RequestMapping(value = "/savePOSAreaMaster", method = RequestMethod.POST)
 	public ModelAndView funAddUpdate(@ModelAttribute("command") @Valid clsPOSAreaMasterBean objBean,BindingResult result,HttpServletRequest req)
@@ -84,15 +83,12 @@ public class clsPOSAreaMasterController {
 		{
 			String clientCode=req.getSession().getAttribute("gClientCode").toString();
 			String webStockUserCode=req.getSession().getAttribute("gUserCode").toString();
-	
 			String areaCode=objBean.getStrAreaCode();
 			
 			if (areaCode.trim().isEmpty())
 			{
 				long intCode =objUtilityController.funGetDocumentCodeFromInternal("Area");
 				areaCode = "A" + String.format("%03d", intCode);
-				
-				
 			}
 
 			clsAreaMasterModel objModel = new clsAreaMasterModel(new clsAreaMasterModel_ID(areaCode, clientCode));
@@ -103,10 +99,7 @@ public class clsPOSAreaMasterController {
 			objModel.setStrDataPostFlag("");
 			objModel.setStrUserCreated(webStockUserCode);
 			objModel.setStrUserEdited(webStockUserCode);
-
 			objMasterService.funSaveUpdateAreaMaster(objModel);
-			
-			
 			req.getSession().setAttribute("success", true);
 			req.getSession().setAttribute("successMessage"," "+areaCode);
 									
@@ -114,48 +107,35 @@ public class clsPOSAreaMasterController {
 		}
 		catch(Exception ex)
 		{
-			
 			ex.printStackTrace();
 			return new ModelAndView("redirect:/frmFail.html");
 		}
 	}
 	
-	//Assign filed function to set data onto form for edit transaction.
-			@RequestMapping(value = "/loadPOSAreaMasterData", method = RequestMethod.GET)
-			public @ResponseBody clsPOSAreaMasterBean funSetSearchFields(@RequestParam("POSAreaCode") String areaCode,HttpServletRequest req)throws Exception
-			{
-				String clientCode=req.getSession().getAttribute("gClientCode").toString();
-				clsPOSAreaMasterBean objPOSAreaMaster = new clsPOSAreaMasterBean();
+//Assign filed function to set data onto form for edit transaction.
+	@RequestMapping(value = "/loadPOSAreaMasterData", method = RequestMethod.GET)
+	public @ResponseBody clsPOSAreaMasterBean funSetSearchFields(@RequestParam("POSAreaCode") String areaCode,HttpServletRequest req)throws Exception
+	{
+		String clientCode=req.getSession().getAttribute("gClientCode").toString();
+		clsPOSAreaMasterBean objPOSAreaMaster = new clsPOSAreaMasterBean();
 				
-				clsAreaMasterModel objModel= (clsAreaMasterModel) objMasterService.funSelectedAreaMasterData(areaCode,clientCode);
-				objPOSAreaMaster.setStrAreaCode(objModel.getStrAreaCode());
-				objPOSAreaMaster.setStrAreaName(objModel.getStrAreaName());
-				objPOSAreaMaster.setStrPOSName(objModel.getStrPOSCode());
-
+		clsAreaMasterModel objModel= (clsAreaMasterModel) objMasterService.funSelectedAreaMasterData(areaCode,clientCode);
+		objPOSAreaMaster.setStrAreaCode(objModel.getStrAreaCode());
+		objPOSAreaMaster.setStrAreaName(objModel.getStrAreaName());
+		objPOSAreaMaster.setStrPOSName(objModel.getStrPOSCode());
 				
-				if(null==objPOSAreaMaster)
-				{
-					objPOSAreaMaster = new clsPOSAreaMasterBean();
-					objPOSAreaMaster.setStrAreaCode("Invalid Code");
-				}
-				
-				return objPOSAreaMaster;
-			}
+		return objPOSAreaMaster;
+	}
 			
-		
-				
-			 @RequestMapping(value ="/checkAreaName" ,method =RequestMethod.GET)
-				public  @ResponseBody boolean funCheckAreaName(@RequestParam("areaName")  String name,@RequestParam("areaCode")  String code,HttpServletRequest req) 
-				{
-					String clientCode =req.getSession().getAttribute("gClientCode").toString();
-
-					int count=objPOSGlobal.funCheckName(name,code,clientCode,"POSAreaMaster");
-
-					if(count>0)
-					 return false;
-					else
-						return true;
-					
-				}
-				
+	
+	@RequestMapping(value ="/checkAreaName" ,method =RequestMethod.GET)
+	public  @ResponseBody boolean funCheckAreaName(@RequestParam("areaName")  String name,@RequestParam("areaCode")  String code,HttpServletRequest req) 
+	{
+		String clientCode =req.getSession().getAttribute("gClientCode").toString();
+		int count=objPOSGlobal.funCheckName(name,code,clientCode,"POSAreaMaster");
+		if(count>0)
+			return false;
+		else
+			return true;
+	}
 }
